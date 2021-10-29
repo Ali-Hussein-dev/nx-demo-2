51ebcd64f4c88a310bf2d23e308d74a4c4e5d3bf
Ali <ali.hussein.pre@gmail.com>
2021-10-29 14:14:47 +0200

    chore(next): install plugins, configure next

--- apps/internal-admin/next.config.js
+++ apps/internal-admin/next.config.js
@@ -1,15 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPlugins = require('next-compose-plugins');
+const withPWA = require('next-pwa');
+const nextConfig = require('../../shared-next-configs.js');
 const withNx = require('@nrwl/next/plugins/with-nx');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
-const nextConfig = {
-  nx: {
-    // Set this to true if you would like to to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: false,
-  },
-};
 
-module.exports = withNx(nextConfig);
+module.exports = withPlugins(
+  [
+    [withBundleAnalyzer],
+    [withNx()],
+    [withPWA({ pwa: { dest: '../../dist/apps/main' } })],
+  ],
+  nextConfig()
+);
