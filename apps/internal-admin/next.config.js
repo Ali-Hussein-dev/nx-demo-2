64ab994667fa764cf47a38fc9df17b2be4c36d1d
Ali <ali.hussein.pre@gmail.com>
2021-10-29 14:31:24 +0200

    chore(internal-admin-next): exclude unnecessary plugin

--- apps/internal-admin/next.config.js
+++ apps/internal-admin/next.config.js
@@ -1,6 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPlugins = require('next-compose-plugins');
-const withPWA = require('next-pwa');
 const nextConfig = require('../../shared-next-configs.js');
 const withNx = require('@nrwl/next/plugins/with-nx');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
@@ -11,11 +10,4 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 
-module.exports = withPlugins(
-  [
-    [withBundleAnalyzer],
-    [withNx()],
-    [withPWA({ pwa: { dest: '../../dist/apps/main' } })],
-  ],
-  nextConfig()
-);
+module.exports = withPlugins([[withBundleAnalyzer], [withNx()]], nextConfig());
